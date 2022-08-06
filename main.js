ec0f28f4b34de662dff05cd9a1973d113e820bf7
unknown <ppatil1071997@gmail.com>
2022-08-06 11:55:07 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// Change the nav style on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
+})
+
+// CONTACT BUTTONS (circular text button)
+
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split("").map((character, index) =>  `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join("")
+});
+
+//
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        599:{
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1023: {
+            slidesPerView: 3,
+            spaceBetween: 60
+        }
+    }
+  });
